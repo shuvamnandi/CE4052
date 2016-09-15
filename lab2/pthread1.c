33c25fdc75fe97c9788ae43fb7b920385fcd23cd
shuvamnandi <shuvamnandi9@gmail.com>
2016-09-15 22:47:38 +0800

    Shifted files into a folder for lab2

---
+++ lab2/pthread1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *print_message(void *ptr)
+{
+	char *text;
+	text = (char *) ptr;
+	printf("%s \n", text);
+}
+
+int main()
+{
+	pthread_t thread1, thread2;
+	char *str1 = "I am THREAD 1";
+	char *str2 = "I am THREAD 2";
+	int T1ret, T2ret;
+	
+	/* Create two threads */ 
+	T1ret = pthread_create(&thread1, NULL, print_message, (void*) str1);
+	T2ret = pthread_create(&thread2, NULL, print_message, (void*) str2);
+	
+	/* main() thread now waits for both threads to finish */
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	printf("T1 & T2 return: %d, %d \n", T1ret, T2ret);
+	return 0;
+}
